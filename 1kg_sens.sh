6b2e65f4dd4f5195bab04ae41f550b4cd20adfdb
Dave Larson <dlarson@genome.wustl.edu>
2017-09-05 10:14:35 -0500

    add general comparison scripts

---
+++ 1kg_sens.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -ueo pipefail
+
+OKG_BEDPE=$1
+EVAL_BEDPE=$2
+
+SLOP=50
+BINDIR=$( dirname $0 )
+
+INTERSECT_SCRIPT=${BINDIR}/filter_based_on_strand.py
+BEDTOOLS=bedtools
+PYTHON=python
+
+TOTAL_1KG=$(cat $OKG_BEDPE | grep -v '^#' | wc -l)
+TOTAL_UNFILTERED=$(cat $EVAL_BEDPE | grep -v '^#' | wc -l)
+TOTAL_FILTERED=$(cat $EVAL_BEDPE | grep -v '^#' | awk '{ if ($12 != "LOW") { print }}' | wc -l)
+OVERLAP_UNFILTERED=$(cat $EVAL_BEDPE | perl -ape 'if($_ !~ /^#/) { $F[1]-=1; $F[2]+=1; $F[4]-=1; $F[5]+=1; @F[1,4] = map { $_ < 0 ? 0 : $_ } ($F[1], $F[4]); $_=join("\t", @F)."\n"}' | $BEDTOOLS pairtopair -is -type both -slop $SLOP -rdn -a $OKG_BEDPE -b stdin | grep -v '^#' | $PYTHON $INTERSECT_SCRIPT -n 22 | cut -f1-11 | sort -u | wc -l)
+OVERLAP_FILTERED=$(cat $EVAL_BEDPE | awk '{ if ($12 != "LOW") { print }}' | perl -ape 'if($_ !~ /^#/) { $F[1]-=1; $F[2]+=1; $F[4]-=1; $F[5]+=1; @F[1,4] = map { $_ < 0 ? 0 : $_ } ($F[1], $F[4]); $_=join("\t", @F)."\n"}' | $BEDTOOLS pairtopair -is -type both -slop $SLOP -rdn -a $OKG_BEDPE -b stdin | grep -v '^#' | $PYTHON $INTERSECT_SCRIPT -n 22 | cut -f1-11 | sort -u | wc -l)
+FRACTION_UNFILTERED=$(awk "BEGIN { print $OVERLAP_UNFILTERED / $TOTAL_UNFILTERED }")
+SENS_UNFILTERED=$(awk "BEGIN { print $OVERLAP_UNFILTERED / $TOTAL_1KG }")
+FRACTION_FILTERED=$(awk "BEGIN { print $OVERLAP_FILTERED / $TOTAL_FILTERED }")
+SENS_FILTERED=$(awk "BEGIN { print $OVERLAP_FILTERED / $TOTAL_1KG }")
+
+echo -e "Unfiltered Total Calls\t$TOTAL_UNFILTERED"
+echo -e "Unfiltered Overlapping 1KG\t$OVERLAP_UNFILTERED"
+echo -e "Unfiltered Fraction Overlapping 1KG\t$FRACTION_UNFILTERED"
+echo -e "Unfiltered 1KG Sensitivity\t$SENS_UNFILTERED"
+echo -e "Filtered Total Calls\t$TOTAL_FILTERED"
+echo -e "Filtered Overlapping 1KG\t$OVERLAP_FILTERED"
+echo -e "Filtered Fraction Overlapping 1KG\t$FRACTION_FILTERED"
+echo -e "Filtered 1KG Sensitivity\t$SENS_FILTERED"
---
+++ 1kg_sens_multi.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -ueo pipefail
+
+OKG_VCF=$1
+EVAL_VCF=$2
+MAP=$3
+
+BINDIR=$( dirname $0 )
+SENS_SCRIPT=${BINDIR}/1kg_sens.sh
+SVTOOLS=svtools
+BCFTOOLS=bcftools
+
+while read INTERNAL EXTERNAL
+do
+    echo $EXTERNAL
+    mkdir $EXTERNAL
+    OKG_BEDPE=$EXTERNAL/1kg_$EXTERNAL.bedpe
+    EVAL_BEDPE=$EXTERNAL/$INTERNAL.var_only.bedpe
+    $BCFTOOLS view -s $EXTERNAL --min-ac 1 $OKG_VCF | $SVTOOLS vcftobedpe | $SVTOOLS bedpesort > $OKG_BEDPE
+    $BCFTOOLS view -s $INTERNAL --min-ac 1 $EVAL_VCF | $SVTOOLS vcftobedpe | $SVTOOLS bedpesort > $EVAL_BEDPE
+    bash $SENS_SCRIPT $OKG_BEDPE $EVAL_BEDPE > $EXTERNAL/$EXTERNAL.1kg.sensitivity
+done < $MAP
+
